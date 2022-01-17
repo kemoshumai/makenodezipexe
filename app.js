113117be74d715c5c118ed2bac251e6a49a92742
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-01-17 16:49:28 +0900

    Runnable

---
+++ app.js
@@ -0,0 +1,42 @@
+const cli = require('cac')();
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs-extra');
+const archiver = require('archiver');
+const child_process = require('child_process');
+
+cli
+    .command('make', 'Make exe for windows.')
+    .option('--src <folderpath>', 'A folder that includes package.json(Default:src)')
+    .option('--unique <string>', 'A blueprint ID to distinguish between files.(Default:Automatic generation with SHA256)')
+    .action((options) => {
+        const sourcefolderpath = path.join(__dirname,(options.src || "./src"));
+        const unique = options.unique || crypto.createHash('sha256').update(crypto.randomBytes(16), 'utf8').digest('hex');
+        console.log("Folder:",sourcefolderpath);
+        console.log("UniqueID:",unique);
+        // Create tmp folder for nexe's source.
+        const tmpfolderpath = path.join(__dirname,"./_tmp");
+        fs.mkdirSync(tmpfolderpath);
+        {
+            const archive = archiver('zip', {
+                zlib: { level: 9 } // 圧縮レベルを指定
+            });
+            const output = fs.createWriteStream(tmpfolderpath + `/${unique}.zip`);
+            archive.pipe(output)
+            archive.directory(sourcefolderpath, '');
+            archive.finalize();
+            output.on('close',()=>{
+                const fileList = fs.readdirSync(path.join(__dirname,"./bundler"));
+                console.log(fileList);
+                for(const file of fileList){
+                    fs.copySync(path.join(__dirname,"./bundler",path.basename(file)),path.join(tmpfolderpath,file));
+                }
+                child_process.execSync(`cd _tmp & nexe bundler.js --output ../dist.exe --target windows-x64-14.15.1 -r "./${unique}.zip" -r "./node.exe" `);
+                fs.remove(tmpfolderpath);
+            });
+        }
+    });
+
+cli.help()
+
+cli.parse()
---
+++ bundler/bundler.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const { spawn } = require('child_process');
+const unzipper = require('unzipper');
+
+const blueprintID = fs.readdirSync(__dirname).filter(f=>f.endsWith('.zip'))?.[0]?.split('.zip')[0];
+const tmpfolderpath = os.tmpdir()
+
+if(!blueprintID){
+    throw 'ZIP Not Found!';
+}
+
+const projectFolder = path.join(tmpfolderpath,blueprintID);
+
+if(!fs.existsSync(projectFolder)){
+    // If not created, unzip.
+    console.log("Extract in ",projectFolder);
+    fs.createReadStream(path.join(__dirname,"./"+blueprintID+".zip")).pipe(unzipper.Extract({ path: projectFolder })).on('close',()=>{
+        console.log("Success!");
+        run();
+    });
+}else{
+    run();
+}
+
+function run(){
+    const indexfile = JSON.parse(fs.readFileSync(path.join(projectFolder,"./package.json")))["main"];
+    if(indexfile == undefined){
+        throw `Not found 'main' on package.json`;
+    }
+
+
+    const child = spawn("node.exe",[path.join(projectFolder,indexfile)],{
+        stdio: 'ignore',
+        detached: true,
+        env: process.env
+    });
+    child.unref();
+}
