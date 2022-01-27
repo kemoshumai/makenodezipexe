b8d3d9ccaebe211f2bb6df8e34a13b6c78465cfe
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-01-27 14:45:05 +0900

    Change system to use zip-unzip-promise

--- bundler/bundler.js
+++ bundler/bundler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const { spawn } = require('child_process');
-const unzipper = require('unzipper');
+const {unzip} = require('zip-unzip-promise');
 
 const blueprintID = fs.readdirSync(__dirname).filter(f=>f.endsWith('.zip'))?.[0]?.split('.zip')[0];
 const tmpfolderpath = os.tmpdir()
@@ -16,13 +16,7 @@ const projectFolder = path.join(tmpfolderpath,blueprintID,"/");
 if(!fs.existsSync(projectFolder)){
     // If not created, unzip.
     console.log("Extract in ",projectFolder);
-    console.log("Zip file:",path.join(__dirname,"./"+blueprintID+".zip"));
-    console.log("Exists:",fs.existsSync(path.join(__dirname,"./"+blueprintID+".zip")));
-    const st = fs.createReadStream(path.join(__dirname,"./"+blueprintID+".zip")).pipe(unzipper.Extract({ path: projectFolder }));
-    st.on('close',()=>{
-        console.log("Success!");
-        run();
-    });
+    unzip(path.join(__dirname,"./"+blueprintID+".zip"), projectFolder).then(run);
 }else{
     run();
 }
